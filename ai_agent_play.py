3fe6d092ff55889eb7b4680c90bed4722bda19ef
Jinglong <jinglongd@iadb.org>
2025-04-20 01:51:10 -0400

    Add tank_dqn_final.h5 model file

--- ai_agent_play.py
+++ ai_agent_play.py
@@ -8,5 +8,6 @@ agent = TankAgent(state_size=5, action_size=5)
 if os.path.exists("tank_dqn_final.h5"):
     agent.load("tank_dqn_final.h5")
     print("loaded model weights: tank_dqn_final.h5")
+    # agent.model.summary()
 
 game.game_running_ai_play(agent)
