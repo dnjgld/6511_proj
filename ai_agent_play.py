474ef958c2cd71d87d16732c6c94ce81c5d6c93f
Jinglong <jinglongd@iadb.org>
2025-04-28 07:50:53 -0400

    Update model weight file reference in ai_agent_play.py to tank_dqn_final_1_35.keras

--- ai_agent_play.py
+++ ai_agent_play.py
@@ -5,9 +5,9 @@ import os
 game = Game()
 agent = TankAgent(state_size=20, action_size=5, epsilon=0.01)
 
-if os.path.exists("tank_dqn_final_1_25.keras"):
-    agent.load("tank_dqn_final_1_25.keras")
-    print("loaded model weights: tank_dqn_final_1_25.keras")
+if os.path.exists("tank_dqn_final_1_35.keras"):
+    agent.load("tank_dqn_final_1_35.keras")
+    print("loaded model weights: tank_dqn_final_1_35.keras")
     # agent.model.summary()
 
 game.game_running_ai_play(agent)
